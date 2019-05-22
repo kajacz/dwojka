90a12f784fc93739014427baaad3e12005762ee0
kajacz <kajaczubala@gmail.com>
2019-05-22 23:52:39 +0200

    nav, onas, cennik

--- hidenavbar.js
+++ hidenavbar.js
@@ -25,27 +25,26 @@ $(window).scroll(example);
 function example() {
   var tempScrollTop = $(window).scrollTop();
 
-
-if((tempScrollTop.toString())>=20){
-  document.getElementById("navbar").style.background = "linear-gradient(#1d1c1b,#1d1c1b)";
-  // rgba(213, 185, 172, 0.753),rgba(213, 185, 172, 0.9)
-  document.getElementById("about").style.display = 'none';
-  document.getElementById("offer").style.display = 'none';
-  document.getElementById("money").style.display = 'none';
-  document.getElementById("contact").style.display = 'none';
-  $("#navbar a").css("font-size","130%")
-  $("#navig").addClass( "ml-auto mr-auto" );
-
+  if (tempScrollTop.toString() >= 20) {
+    document.getElementById("navbar").style.background =
+      "linear-gradient(#ebd8d0,#ebd8d0)";
+    // rgba(213, 185, 172, 0.753),rgba(213, 185, 172, 0.9)
+    document.getElementById("about").style.display = "none";
+    document.getElementById("offer").style.display = "none";
+    document.getElementById("money").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+    $("#navbar a").css("font-size", "130%");
+    $("#navig").addClass("ml-auto mr-auto");
+  } else {
+    document.getElementById("navbar").style.background =
+      "linear-gradient(transparent, transparent)";
+    document.getElementById("about").style.display = "block";
+    document.getElementById("offer").style.display = "block";
+    document.getElementById("money").style.display = "block";
+    document.getElementById("contact").style.display = "block";
+    $("#navig").removeClass("mr-auto");
+    $("#navbar a").css("font-size", "130%");
+    // $('.navbar').fadeOut();
+    // $('.navbar').fadeIn();
+  }
 }
-else{
-  document.getElementById("navbar").style.background = "linear-gradient(transparent, transparent)";
-  document.getElementById("about").style.display = 'block';
-  document.getElementById("offer").style.display = 'block';
-  document.getElementById("money").style.display = 'block';
-  document.getElementById("contact").style.display = 'block';
-  $("#navig").removeClass( "mr-auto" );
-  $("#navbar a").css("font-size","130%")
-  // $('.navbar').fadeOut();
-  // $('.navbar').fadeIn();
-}
-};
--- moveTo.js
+++ moveTo.js
@@ -1,7 +1,21 @@
 function moveTo(target) {
-
-    window.scrollTo(0, target);
+  window.scrollTo(0, target);
 }
 
+window.onscroll = function() {
+  scrollFunction();
+};
 
- 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
