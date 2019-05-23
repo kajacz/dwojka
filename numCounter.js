41969bb41746a8ba4cdec9ee8889ba64340e0974
kajacz <kajaczubala@gmail.com>
2019-05-24 00:07:57 +0200

    responsive map, counting up fixed

--- numCounter.js
+++ numCounter.js
@@ -1,7 +1,13 @@
 // $(".num").counterUp({delay:10, time:1000});
-jQuery(document).ready(function(){
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
+// jQuery(document).ready(function() {
+//   $(".counter").counterUp({
+//     delay: 10,
+//     time: 1000
+//   });
+// });
+jQuery(document).ready(function($) {
+  $(".counter").counterUp({
+    delay: 10,
+    time: 1000
+  });
 });
